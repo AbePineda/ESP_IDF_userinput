d63238fa48e575b59dae9e03404289f497923f78
AbePineda <abepineda07@gmail.com>
2025-06-24 18:01:33 -0700

    Create button_matrix_example.c

---
+++ examples/button_matrix_example.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+
+#include "esp_adc/adc_continuous.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
+#include "esp_adc/adc_oneshot.h"
+#include "hal/adc_types.h"
+
+#include "driver/ledc.h"
+
+#include "DIGITAL_INPUT.h"
+#include "ANALOG_INPUT.h"
+
+//2x2 matrix
+gpio_num_t myrow[] = {GPIO_NUM_6, GPIO_NUM_7};
+gpio_num_t mycolumn[] = {GPIO_NUM_18, GPIO_NUM_8};
+
+button_matrix_t mymatrix;
+
+void app_main() {
+  MATRIX_CONFIG(&mymatrix, myrow, 2, mycolumn, 2); // row/columns and # of elements on each
+  while(1) {
+    MATRIX_UPDATE(&mymatrix);
+    if(matrix_get_state(&mymatrix, 0, 0) == BUTTON_PRESSED) {
+      printf("Button at row 0, col 0 pressed \n"); //0 indexing, add 1 if you want 
+    }
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
